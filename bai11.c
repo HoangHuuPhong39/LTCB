59b0989a5008af2800e76b4221b69ebafe6861ff
HHPhong <phongluzy0902@gmail.com>
2025-03-06 08:22:47 +0700

    Add files via upload

---
+++ bai11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ int main(){
+    float R, V, S, pi=3.14;
+    printf("nhap vao ban kinh cua hinh tron va hinh cau: ");
+    scanf("%f", &R);
+    printf("dien tich cua hinh tron la: %f va the tich cua hinh cau la: %f", S=pi*R*R, V=(4*pi*R*R*R)/3);
+    return 0;
+ }
+
